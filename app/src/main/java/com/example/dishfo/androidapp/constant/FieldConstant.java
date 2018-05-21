127b20d09392f32ee51a693e4c4ce97d5720df86
bluegrey <184016480@qq.com>
2018-05-21 16:44:40 +0800

    constant

---
+++ app/src/main/java/com/example/dishfo/androidapp/constant/FieldConstant.java
@@ -0,0 +1,56 @@
+package com.example.dishfo.androidapp.constant;
+
+/**
+ * Created by dishfo on 18-2-22.
+ */
+
+public class FieldConstant {
+    public static String oldUserName="oldUserName";
+    public static String oldUserNamerev="deer";
+    public static String level="level";
+    public static String levelrev="duck";
+    public static String password="password";
+    public static String passwordrev="cat";
+    public static String discussNumber="discussNumber";
+    public static String discussNumberrev="mother";
+    public static String discuessContent="discuessContent";
+    public static String discuessContentrev="wolf";
+    public static String readNumber="readNumber";
+    public static String readNumberrev="monkey";
+    public static String followAreaId="followAreaId";
+    public static String followAreaIdrev="goose";
+    public static String head="head";
+    public static String headrev="fish";
+    public static String oldImages="oldImages";
+    public static String oldImagesrev="leopard";
+    public static String discussUserEmail="discussUserEmail";
+    public static String discussUserEmailrev="tortoise";
+    public static String time="time";
+    public static String timerev="sheep";
+    public static String images="images";
+    public static String imagesrev="lion";
+    public static String followUserEmail="followUserEmail";
+    public static String followUserEmailrev="rat";
+    public static String oldContent="oldContent";
+    public static String oldContentrev="bear";
+    public static String areaId="areaId";
+    public static String areaIdrev="hit";
+    public static String appreciateNumber="appreciateNumber";
+    public static String appreciateNumberrev="hedgehog";
+    public static String id="id";
+    public static String idrev="snake";
+    public static String email="email";
+    public static String emailrev="dog";
+    public static String fansUserEmail="fansUserEmail";
+    public static String fansUserEmailrev="tiger";
+    public static String content="content";
+    public static String contentrev="cattle";
+    public static String autograph="autograph";
+    public static String autographrev="pig";
+    public static String noteId="noteId";
+    public static String noteIdrev="rabbit";
+    public static String areaName="areaName";
+    public static String areaNamerev="chicken";
+    public static String name="name";
+    public static String namerev="whale";
+}
